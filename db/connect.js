b9f43c9516e9ea1aafa687820761faeb2cb78947
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-15 18:13:48 -0700

    Add MongoDB connection and environment configuration

---
+++ db/connect.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error("Error connecting to MongoDB", err))
+  .finally(() => {
+    client.close();
+  });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
 const nameRoutes = require('./routes/nameRoutes');
 
 
